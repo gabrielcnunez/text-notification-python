291cb01aadf256a3a32179a5b51a79c389b2fb7b
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-02-01 17:05:31 -0700

    Add get all templates endpoint

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+@app.route('/template', methods=['GET'])
+def index():
+    conn = get_db_connection()
+    templates = conn.execute('SELECT * FROM templates').fetchall()
+    conn.close()
+    template_list = []
+    for template in templates:
+        template_list.append({"body": template["body"]})
+
+    return {"templates": template_list}
+        
