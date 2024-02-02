e2cee29dc7c77b82a64c1a8225ec73a580f83eb6
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-02-01 21:38:32 -0700

    Refactor update template endpoint to pass sad path tests

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def create_template():
     return jsonify({'id': template_id, 'body': template_data['body']}), 201
 
 @app.route('/template/<int:id>', methods=['PUT'])
-def update_template(id):
+def update_template_put(id):
     conn = get_db_connection()
 
     existing_template = conn.execute('SELECT * FROM templates WHERE id = ?', (id,)).fetchone()
@@ -57,6 +57,10 @@ def update_template(id):
         return 'Template not found', 404
 
     updated_template_data = request.json
+
+    if 'body' not in updated_template_data or not updated_template_data['body'].strip():
+        conn.close()
+        return 'Template body cannot be blank', 400
 
     query = 'UPDATE templates SET body = ? WHERE id = ?'
     conn.execute(query, (updated_template_data['body'], id))
